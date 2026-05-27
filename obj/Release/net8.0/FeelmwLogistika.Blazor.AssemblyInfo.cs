4fd9f861a8379effacd676d01818684dc2dc3a0e
feelmw <kaixo@feelmw.com>
2026-05-27 13:36:39 +0200

    Fix GitHub Pages base href and css paths

--- obj/Release/net8.0/FeelmwLogistika.Blazor.AssemblyInfo.cs
+++ obj/Release/net8.0/FeelmwLogistika.Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FeelmwLogistika.Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83a66ba9a75fd49d2bd701c789317c4cfb6b313")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76d74e5bc2bcd0e3e034c37e1fb6e2b4468a5472")]
 [assembly: System.Reflection.AssemblyProductAttribute("FeelmwLogistika.Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FeelmwLogistika.Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
