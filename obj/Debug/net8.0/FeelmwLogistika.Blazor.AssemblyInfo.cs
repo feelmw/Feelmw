3a745433d10ef757853841269742deaa4bee6f46
feelmw <kaixo@feelmw.com>
2026-05-27 14:58:40 +0200

    Formulategiak txukunago ikusteko

--- obj/Debug/net8.0/FeelmwLogistika.Blazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/FeelmwLogistika.Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FeelmwLogistika.Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08488e3bb39dc3e4cdeb417523b0136e003915f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dbea69f1a0477c8aa2b7d46b43cfa396fb413ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("FeelmwLogistika.Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FeelmwLogistika.Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
